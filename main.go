3a1ce1082aeb1e9e972e45121440ec362d1c490a
HenriqueLimas <henrique.ramos.limas@gmail.com>
2017-05-23 19:22:03 -0300

    Add rate api

--- main.go
+++ main.go
@@ -1,17 +1,39 @@
 package main
 
 import (
-	"io"
+	"io/ioutil"
+	"log"
 	"net/http"
 	"os"
 )
 
-func hello(w http.ResponseWriter, r *http.Request) {
-	io.WriteString(w, "Hello World!")
+func rate(w http.ResponseWriter, r *http.Request) {
+	const rate_api = "http://api.promasters.net.br/cotacao/v1/valores"
+	base := r.URL.Query().Get("base")
+	log.Println(r.Method, r.URL)
+
+	if base == "" {
+		base = "USD"
+	}
+
+	res, err := http.Get(rate_api + "?alt=json&moedas=" + base)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	body, err := ioutil.ReadAll(res.Body)
+	res.Body.Close()
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	w.Write(body)
 }
 
 func main() {
 	port := os.Getenv("PORT")
-	http.HandleFunc("/", hello)
+	http.HandleFunc("/rates", rate)
 	http.ListenAndServe(":"+port, nil)
 }
